d86ac9ad75223f6d4ee05f06acb667b3bcc867a6
Aysllan Santos da Silva Pereira <Aysllan53642626@edu.df.senac.br>
2025-09-02 21:04:52 -0300

    Alterando o JS do Front e Back

--- Back/app.js
+++ Back/app.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path')
 const port = 3000;
 
 const app = express();
@@ -8,13 +10,29 @@ app.use(cors());
 app.use(express.json());
 // app.request()
 
-app.post('/', (req, res) => {
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+app.post('/', upload.single('file'), (req, res) => {
+    console.log(req.body);
     const { nome, cpf, endereco, turno, curso, numeroTurma } = req.body;
-    res.json({status: 'ok'});
-    console.log(`Recebidos! ${nome, cpf, endereco, turno, curso, numeroTurma}`);
-    res.send('ok');
+    const arquivo = req.file;
+    console.log(req.file);
+
+    res.json({status: 'ok', file: arquivo?.filename});
+    console.log(`Recebidos: Nome: ${nome}, CPF: ${cpf}, Endereco: ${endereco}, Turno: ${turno}, Curso: ${curso}, Numero: ${numeroTurma}`);
+    console.log(`Arquivo: `, arquivo);
+    // res.send('ok');
 });
 
 app.listen(3000, () => {
     console.log(`Servidor rodando na porta ${port}`);
-})
+});
